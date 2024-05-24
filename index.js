ed8aed4248a517c675dc4994b41d425c443e4543
Matari7 <arimcf10@hotmail.es>
2024-05-24 13:30:12 -0500

    consola

--- index.js
+++ index.js
@@ -5,10 +5,7 @@ const API_URL = 'https://jsonplaceholder.typicode.com/';
 axios.get(`${API_URL}/users`)
   .then(response => {
     const data = response.data;
-    const HTMLResponse = document.querySelector("#app");
-
-    const template = data.map(user => `<li>${user.name} || el correo es: || ${user.email}</li>`);
-    HTMLResponse.innerHTML = `<ul>${template}</ul>`;
+    console.log(data);
   })
   .catch(error => {
     console.error('Error al hacer la solicitud:', error);
