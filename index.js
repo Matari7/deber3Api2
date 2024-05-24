7d0f53657105c366d7d3b0d90eedad8539265602
Matari7 <arimcf10@hotmail.es>
2024-05-24 12:38:57 -0500

    Update index.js axios
    con axios

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
+const axios = require('axios');
+
 const API_URL = 'https://jsonplaceholder.typicode.com/';
 
-const xhr = new XMLHttpRequest();
+axios.get(`${API_URL}/users`)
+  .then(response => {
+    const data = response.data;
+    const HTMLResponse = document.querySelector("#app");
 
-function onRequestHandler() {
-    if(this.readyState == 4 && this.status == 200){
-        // 0 = UNSET, no se ha llamado al metodo open
-        // 1 = OPENED, se ha llamado al metodo open
-        // 2 = HEADERS_RECEIVED, se esta llamando al metodo send()
-        // 3 = LOADING, esta cargando, es decir, esta recibiendo la respuesta
-        // 4 = DONE, se ha completado la operacion
-       const data = JSON.parse(this.response);
-       const HTMLResponse = document.querySelector("#app");
-
-       const template = data.map(user => `<li>${user.name} || el correo es: || ${user.email}</li>`);
-       HTMLResponse.innerHTML = `<ul>${template}</ul>`;
-    }
-}
-
-xhr.addEventListener('load', onRequestHandler);
-xhr.open("GET", `${API_URL}/users`);
-xhr.send();
+    const template = data.map(user => `<li>${user.name} || el correo es: || ${user.email}</li>`);
+    HTMLResponse.innerHTML = `<ul>${template}</ul>`;
+  })
+  .catch(error => {
+    console.error('Error al hacer la solicitud:', error);
+  });
